4ad344a8d61af733356caadbf2552a193a5b6716
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-23 12:38:17 +0530

    Add: File base route test

---
+++ src/posts/error.js
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+
+const Error = () => {
+    return /*#__PURE__*/React.createElement("h1", null, "Error");
+};
+export default Error; // Path: src/posts/Posts.jsx
