ae0a477e35a5267bd5bfa5119b1c2ef68c57b453
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-23 12:59:12 +0530

    Add: API route

--- src/posts/error.js
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-
-const Error = () => {
-    return /*#__PURE__*/React.createElement("h1", null, "Error");
-};
-export default Error; // Path: src/posts/Posts.jsx
