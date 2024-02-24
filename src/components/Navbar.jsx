4b6e6143869c622a72b7632b134d3fc33a110d21
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-24 18:44:57 +0530

    Add: Large devices navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,20 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="flex justify-between items-center w-full mb-0 pt-0">
+      <Link
+        href={"/"}
+        className="flex justify-center items-center gap-2"
+      >
+        <Image
+          src={"/assets/images/promptalley-logos_white.png"}
+          width={160}
+          height={160}
+          className="bg-contain w-44 h-44 -my-8"
+        >
+        </Image>
+      </Link>
+    </nav>
   )
 }
 
