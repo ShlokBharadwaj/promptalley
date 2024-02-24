5a7e4bc1879df1bd41a7b3350ae2a339c765ea0a
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-24 13:29:14 +0530

    Add: Landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,1 @@
-const Home = () => {
-  return (
-    <section className="w-full flex justify-center items-center flex-col">
-      <h1 className="mt-5 text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl text-center">Find and Share
-        <br />
-        <span className="bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent text-center">AI-curated prompts</span>
-      </h1>
-    </section>
-  )
-}
-
-export default Home;
+export { default } from './App';
