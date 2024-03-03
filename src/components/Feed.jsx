54d224f92276ff564e366e295839819e162a1e5a
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-03-03 19:25:27 +0530

    Add: Search functionality

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -9,6 +9,7 @@ const Feed = () => {
 
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
 
   const handleSearchChange = (e) => {
     setSearchText(e.target.value);
@@ -25,6 +26,17 @@ const Feed = () => {
     fetchPosts();
   }, []);
 
+  useEffect(() => {
+    console.log("Posts are: ", posts);
+    setFilteredPosts(
+      posts.filter(post =>
+        (post.tags && post.tags.some(tag => tag.toLowerCase().includes(searchText.toLowerCase()))) ||
+        (post.user && post.user.toLowerCase().includes(searchText.toLowerCase())) ||
+        (post.prompt && post.prompt.toLowerCase().includes(searchText.toLowerCase()))
+      )
+    );
+  }, [searchText, posts]);
+
   return (
     <section className="mt-16 mx-auto w-full max-w-xl flex justify-center items-center flex-col gap-2">
       <form className="relative w-full flex justify-center items-center">
@@ -39,8 +51,10 @@ const Feed = () => {
       </form>
 
       <PromptCardList
-        data={posts}
-        handleTagClick={() => { }}
+        data={filteredPosts}
+        handleTagClick={(tag) => {
+          setSearchText(tag);
+        }}
       />
     </section>
   )
