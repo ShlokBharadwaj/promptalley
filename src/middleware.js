ba33ebe30ba632828233c0b58dd2288d43181752
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-03-09 13:25:11 +0530

    Add: Protect /profile route

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/create-prompt", "/update-prompt", "/api/auth"] }
+export const config = { matcher: ["/create-prompt", "/update-prompt", "/api/auth", "/profile"] }
