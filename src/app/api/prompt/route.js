720e7a9b76b47d5b4c080d36877006990b4a506b
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-03-10 13:00:01 +0530

    Fix: Import populating model for caching

--- src/app/api/prompt/route.js
+++ src/app/api/prompt/route.js
@@ -1,14 +1,12 @@
 import { connectToDatabase } from '@/utils/database';
 import Prompt from "@/models/prompt";
+import User from "@/models/user";
 
 export const GET = async (req, res) => {
     try {
         await connectToDatabase();
 
         const prompts = await Prompt.find({}).populate("creator");
-
-        // Set the Cache-Control header to no-store in the response, to prevent caching.
-        res.setHeader('Cache-Control', 'no-store');
 
         return new Response(JSON.stringify(prompts), {
             status: 200,
